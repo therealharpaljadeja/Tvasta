9546ecac825869308fecb0bfe64687c4d34d01eb
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2020-02-13 19:35:22 +0530

    Create a root file for css and modulerization

--- src/index.js
+++ src/index.js
@@ -1,50 +1,36 @@
-const expand = (element) => {
+const expand = (element) => { 
 	// remove active class from any element.
 	options.forEach(el => {
-		el.classList.remove('option-active');
+		el.classList.remove('how_it_works__grid__option-active');
 	})
 
 	// add active class to the element that is clicked
 	let abc = element.target;
-	let isOption = Array.from(abc.classList).indexOf('option');
+	let isOption = Array.from(abc.classList).indexOf('how_it_works__grid__option');
 	if(isOption === -1){
  		abc = abc.parentElement
- 		abc.classList.add('option-active');
+ 		abc.classList.add('how_it_works__grid__option-active');
 	} else {
-		abc.classList.add('option-active');
+		abc.classList.add('how_it_works__grid__option-active');
 	}
 
 	// remove expand class from any element.
 	option_desc.forEach(el => {
-		el.classList.remove('option-desc-expand');
+		el.classList.remove('how_it_works__grid__option__desc-expand');
 	});
 
 	// apply expand class to the appropriate element.
-	abc.nextElementSibling.classList.add('option-desc-expand');
+	abc.nextElementSibling.classList.add('how_it_works__grid__option__desc-expand');
 
 	// Below will execute only when the screen size is mobile.
-	if(screen.width <= 375){
-		// Get grid row and grid column
-		let grid_row_start = window.getComputedStyle(abc)['grid-row-end'];	
-		let grid_column_start = window.getComputedStyle(abc)['grid-column-end'];
-
-		// apply grid row and grid column to the expanding element.
-		window.getComputedStyle(abc.nextElementSibling)['grid-row-start'] = grid_row_start;
-		window.getComputedStyle(abc.nextElementSibling)['grid-row-end'] = grid_row_start + 1;
-		window.getComputedStyle(abc.nextElementSibling)['grid-column-start'] = grid_column_start;
-		window.getComputedStyle(abc.nextElementSibling)['grid-column-end'] = grid_column_start + 1;
-
-
-	 
-	}
+	
 
 	
 }
 
-let options = Array.from(document.querySelectorAll('.option'));
-let option_desc = Array.from(document.querySelectorAll('.option-desc'));
+let options = Array.from(document.querySelectorAll('.how_it_works__grid__option'));
+let option_desc = Array.from(document.querySelectorAll('.how_it_works__grid__option__desc'));
 
-console.log(option_desc);
 options.forEach(el => {
 			el.addEventListener('click', expand);
 });

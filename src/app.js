dd905b60be8ce9ad7571f2f99ab680c210bfbce4
Harpal Jadeja <jadejaharpal14@gmail.com>
2020-03-26 13:26:21 +0530

    Hospital Details Page added

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const ejs = require('ejs');
 const fs = require('fs');
+const path = require('path');
 app = express();
 app.set('views', __dirname);
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname)));
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-	res.render('index.html');
+	res.render('views/index.html');
 });
 
 const dev_data_string = fs.readFileSync('dev_data.json').toString();
@@ -16,11 +17,11 @@ const dev_data_json = JSON.parse(dev_data_string);
 
 
 app.get('/doctors', (req, res) => {
-	res.render('doctor.ejs', doctors = dev_data_json.doctors);
+	res.render('views/doctor.ejs', doctors = dev_data_json.doctors);
 })
 
 app.get('/hospitals', (req, res) => {
-	res.render('hospital.ejs', hospital = dev_data_json.hospitals);
+	res.render('views/hospital.ejs', hospital = dev_data_json.hospitals);
 })
 
 module.exports = app;

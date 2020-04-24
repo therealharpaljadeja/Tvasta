dd7758f6c0af461e448aa32e1057646f6c80349c
Harpal Jadeja <jadejaharpal14@gmail.com>
2020-04-24 19:49:27 +0530

    Slots appear in frontend and corner cases are resolved

--- src/controllers/slotController.js
+++ src/controllers/slotController.js
@@ -1,29 +1,107 @@
 const Slot = require('./../models/slotModel.js');
 
 const addSlot = async (req, res, next) => {
-	let intervalString = req.body.interval.slice(1,req.body.interval.length - 1).split(',');
-	let interval = [];
-	let hospitals = req.body.hospital.slice(1,req.body.hospital.length - 1).split(',');
-	let hospital = [];
-	for(let i = 0; i < hospitals.length; i++){
-        value = JSON.parse(hospitals[i]).value;
-        hospital.push(value);
-    }
-	for(let i = 0; i < intervalString.length; i++){
-        value = JSON.parse(intervalString[i]).value;
-        interval.push(value);
-    }
-	console.log(req.body);
-	const slot = await Slot.create({
-		startTime: req.body.startTime,
-		endTime: req.body.endTime,
-		day: req.body.day,
-		interval: parseInt(interval[0]),
-		hospital: hospital[0],
-		holiday: req.body.holiday ? true : false,
-		doctor: req.session.user._id
+	const allSlots = await Slot.find({ 
+		$and: [
+			{
+				doctor: req.session.user._id
+			},
+			{
+				holiday: false
+			},
+			{
+				isDisabled: false
+			}
+		]
 	});
-	res.redirect('/schedule-appointment');	
+	if(allSlots.length){
+		const valid = await isValidateSlot(allSlots, req.body);
+		console.log(valid);
+		if(valid){
+			let intervalString = req.body.interval.slice(1,req.body.interval.length - 1).split(',');
+			let interval = [];
+			let hospitals = req.body.hospital.slice(1,req.body.hospital.length - 1).split(',');
+			let hospital = [];
+			for(let i = 0; i < hospitals.length; i++){
+		        value = JSON.parse(hospitals[i]).value;
+		        hospital.push(value);
+		    }
+			for(let i = 0; i < intervalString.length; i++){
+		        value = JSON.parse(intervalString[i]).value;
+		        interval.push(value);
+		    }
+			console.log(req.body);
+			const slot = await Slot.create({
+				startTime: req.body.startTime,
+				endTime: req.body.endTime,
+				day: req.body.day,
+				interval: parseInt(interval[0]),
+				hospital: hospital[0],
+				holiday: req.body.holiday ? true : false,
+				doctor: req.session.user._id
+			});
+			req.session.error = 'Slot Succesfully Added';
+			req.session.errorType = 'Success';
+			res.redirect('/schedule-appointment');		
+		} else {
+			req.session.error = "Time Overlap";
+			req.session.errorType = 'Failure';
+			res.redirect('/schedule-appointment');
+		}
+	} else {
+		let intervalString = req.body.interval.slice(1,req.body.interval.length - 1).split(',');
+		let interval = [];
+		let hospitals = req.body.hospital.slice(1,req.body.hospital.length - 1).split(',');
+		let hospital = [];
+		for(let i = 0; i < hospitals.length; i++){
+	        value = JSON.parse(hospitals[i]).value;
+	        hospital.push(value);
+	    }
+		for(let i = 0; i < intervalString.length; i++){
+	        value = JSON.parse(intervalString[i]).value;
+	        interval.push(value);
+	    }
+		console.log(req.body);
+		const slot = await Slot.create({
+			startTime: req.body.startTime,
+			endTime: req.body.endTime,
+			day: req.body.day,
+			interval: parseInt(interval[0]),
+			hospital: hospital[0],
+			holiday: req.body.holiday ? true : false,
+			doctor: req.session.user._id
+		});
+		req.session.error = 'Slot Succesfully Added';
+		req.session.errorType = 'Success';
+		res.redirect('/schedule-appointment');
+	}
+	
+	
+		
+}
+
+const isValidateSlot = async (slots, slot) => {
+	for(let i = 0; i < slots.length; i++){
+		console.log(slots[i].day.toLowerCase(),slot.day)
+		if(slots[i].day.toLowerCase() === slot.day){
+			console.log(slots[i].startTime > slot.startTime, slots[i].startTime > slot.startTime);
+			if(slots[i].startTime > slot.startTime && slots[i].endTime < slot.endTime){
+				return false;
+			} else {
+				if(slots[i].startTime > slot.startTime && slots[i].endTime > slot.endTime && slots[i].startTime < slot.endTime){
+					return false;
+				} else {
+					if(slots[i].startTime < slot.startTime && slots[i].endTime > slot.startTime && slots[i].endTime < slot.endTime){
+						return false;
+					} else {
+						return true;
+					}
+				}
+			}
+		} else {
+			return true;
+		}
+	}
 }
 
 const getSlotsBasedOnDoctor = async (req, res, next) => {
--- src/models/slotModel.js
+++ src/models/slotModel.js
@@ -19,39 +19,39 @@ const slotSchema = new mongoose.Schema({
 	holiday: {
 		type: Boolean,
 		default: false
+	},
+	isDisabled: {
+		type: Boolean,
+		default: false
 	}
 },{ timestamps: true });
 
 
 slotSchema.pre('save',async function(next){
 
-	function ampmto24hours(ampmtime) {
-		var hours = parseInt(ampmtime.split(':')[0]);
-		var minutes = parseInt(ampmtime.split(':')[1]);
-		console.log(ampmtime);
-		var AMPM = ampmtime.slice(5,ampmtime.length);
-		console.log(AMPM);
-		if(AMPM == "PM" && hours<12) hours = hours+12;
-		if(AMPM == "AM" && hours==12) hours = hours-12;
-		var sHours = hours.toString();
-		var sMinutes = minutes.toString();
-		if(hours<10) sHours = "0" + sHours;
-		if(minutes<10) sMinutes = "0" + sMinutes;
-		return `${sHours}:${sMinutes}`;
+	// function ampmto24hours(ampmtime) {
+	// 	var hours = parseInt(ampmtime.split(':')[0]);
+	// 	var minutes = parseInt(ampmtime.split(':')[1]);
+	// 	var AMPM = ampmtime.slice(5,ampmtime.length);
+	// 	if(AMPM == "PM" && hours<12) hours = hours+12;
+	// 	if(AMPM == "AM" && hours==12) hours = hours-12;
+	// 	var sHours = hours.toString();
+	// 	var sMinutes = minutes.toString();
+	// 	if(hours<10) sHours = "0" + sHours;
+	// 	if(minutes<10) sMinutes = "0" + sMinutes;
+	// 	return `${sHours}:${sMinutes}`;
 	
-	}
+	// }
 	
-	let startTime = ampmto24hours(this.startTime);
-	let endTime = ampmto24hours(this.endTime);
-	console.log(startTime);
-	console.log(endTime);
+	// let startTime = ampmto24hours(this.startTime);
+	// let endTime = ampmto24hours(this.endTime);
 
 	const start = new Date();
-	start.setHours(parseInt(startTime.split(':')[0]));
-	start.setMinutes(parseInt(startTime.split(':')[1]));
+	start.setHours(parseInt(this.startTime.split(':')[0]));
+	start.setMinutes(parseInt(this.startTime.split(':')[1]));
 	const end = new Date();
-	end.setHours(parseInt(endTime.split(':')[0]));
-	end.setMinutes(parseInt(endTime.split(':')[1]));
+	end.setHours(parseInt(this.endTime.split(':')[0]));
+	end.setMinutes(parseInt(this.endTime.split(':')[1]));
 	
 	const slots = [];
 	const slotsWithDate = [];
@@ -60,11 +60,6 @@ slotSchema.pre('save',async function(next){
 		let endTime = end.getTime();
 		let duration = parseInt(interval) * 60 * 1000;
 		let slotEndTime = start.getTime();
-		console.log(start);
-		console.log(end);
-		console.log(startTime);
-		console.log(endTime);
-		console.log(duration);
 		while(startTime < endTime){
 			slotEndTime += duration;
 			let startDate = new Date(startTime);
